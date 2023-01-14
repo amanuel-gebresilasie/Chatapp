ee72ff8bc3bcb5035a4797d8079af32f195cdc09
amanuel-gebresilasie <72979298+amanuel-gebresilasie@users.noreply.github.com>
2023-01-14 10:26:18 +0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+CSRF_TRUSTED_ORIGINS = ['https://chatapp-production-0a81.up.railway.app']
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
